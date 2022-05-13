8e08b34bd849af276dfdc15c95d1f207f887a898
Kaizen Conroy <zen.conroy@gmail.com>
2022-05-13 15:57:37 -0400

    more boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { loadActionOptions } from './options';
 
 async function run() {
+  console.log('here');
   const options = loadActionOptions();
   console.log(options);
+
+  const client = github.getOctokit(options.githubToken);
+  const owner = github.context.repo.owner;
+  const repo = github.context.repo.repo;
+  await client.rest.issues.get({
+    issue_number: 1,
+    owner,
+    repo,
+  });
 }
 
 run().catch(error => {
+  console.log(error);
   core.setFailed(error.message);
 });
